4fee05cfbcc56228143198a3f981a20078d90b03
YoonseoJ <cassie961206@gmail.com>
2023-11-21 21:02:27 -0800

    update Landing responsive

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -14,19 +14,19 @@ const font = EB_Garamond({
 export default function Landing() {
     
     return (
-        <div className='relative flex h-[40rem]'>
+        <div className='relative flex h-[40rem] lg:h-[45rem]'>
             <Image src={bg_paper}
                 alt='Pictures of the author' 
                 className='mx-auto w-full object-cover absolute h-full'
                 priority
             />
-            <div className='flex flex-col items-start mt-20 gap-6 md:flex-row md:items-center'>
+            <div className='w-full flex flex-col items-center mt-20 gap-6 md:flex-row md:items-center md:w-fit'>
                 <Image src={Mascot}
                     alt='Pictures of the author' 
-                    className='w-60 object-contain brightness-165 ml-8 mt-20 md:mt-0 md:ml-10 md:w-72 lg:w-96'
+                    className='w-60 object-contain brightness-165 mt-20 md:mt-0 md:ml-10 md:w-72 lg:w-96 lg:ml-20 xl:ml-32'
                     priority
                 />
-                <div className=' w-full -mt-10 z-10 ml-10 md:ml-0'>
+                <div className=' w-fit -mt-10 z-10 flex flex-col items-center md:items-start md:w-full'>
                     <div className=' w-64 pt-10 pb-5 md:w-80 lg:w-96'>
                         <Image src={Logo}
                             alt='Picture of the dooboo logo' 
@@ -34,7 +34,7 @@ export default function Landing() {
                             priority
                         />
                     </div>
-                    <p className={`text-neutral-800 text-2xl ml-3 md:text-3xl lg:text-4xl ${font.className}`}>
+                    <p className={`text-neutral-800 text-2xl md:ml-3 md:text-3xl lg:text-4xl ${font.className}`}>
                         Taste and Authenticity of Korea
                     </p>
                 </div>
