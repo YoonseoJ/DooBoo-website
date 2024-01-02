b8d3b12bfee181166399418c79725f505de91e8d
Yoonseo Jeong <cassie961206@gmail.com>
2024-01-01 19:37:10 -0800

    apply animation to landing page and menu detail modal

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,21 +1,39 @@
+'use client'
+
 import Image from 'next/image';
 import Mascot from '../../public/logoimg.png'
 import Logo from '../../public/logo_b.png'
 import { EB_Garamond } from 'next/font/google';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { gsap } from 'gsap'
 
 const font = EB_Garamond({
     subsets: ['latin'],
     weight: '400'
 })
 
+
 export default function Landing() {
     const [loading, setLoading] = useState(true)
 
+    const tl = gsap.timeline({defaults: {duration: 0.75, ease: 'power1.out'}})
+
     function onImageLoad() {
         setLoading(false)
-        console.log("loaded")
     }
+
+    useEffect(() => {
+        const hasWindow = typeof window !== 'undefined';
+        const width = hasWindow ? window.innerWidth : null;
+
+        tl.fromTo('#mascot', {scale: 0}, {scale: 1, ease: "power2.out", duration: 1})
+        if(width! > 768) {
+            tl.fromTo('#landingText', {x: 70, opacity:0}, {x:0, opacity:1 , duration: 1}, '<')
+        } else {
+            tl.fromTo('#landingText', {y: 70, opacity:0}, {y:0, opacity:1 , duration: 1}, '<')
+        }
+        
+    }, [tl])
     
     return (
         <div className='relative flex h-[40rem] lg:h-[45rem]'>
@@ -33,11 +51,12 @@ export default function Landing() {
             />
             <div className='w-full flex flex-col items-center mt-20 gap-6 md:flex-row md:items-center md:w-fit'>
                 <Image src={Mascot}
+                    id='mascot'
                     alt='Pictures of the author' 
                     className='w-60 object-contain brightness-100 mt-20 md:mt-0 md:ml-10 md:w-72 lg:w-96 lg:ml-20 xl:ml-32'
                     priority
                 />
-                <div className=' w-fit -mt-10 z-10 flex flex-col items-center md:items-start md:w-full'>
+                <div id='landingText' className=' w-fit -mt-10 z-10 flex flex-col items-center md:items-start md:w-full'>
                     <div className=' w-64 pt-10 pb-5 md:w-80 lg:w-96'>
                         <Image src={Logo}
                             alt='Picture of the dooboo logo' 
--- src/components/MenuModal.tsx
+++ src/components/MenuModal.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { gsap } from 'gsap'
 import CloseIcon from './ui/icons/CloseIcon'
+import { useEffect } from 'react'
 
 type Props = {
     children: React.ReactNode
@@ -6,6 +10,12 @@ type Props = {
 }
 
 export default function MenuModal({onClose, children}: Props) {
+
+    const tl = gsap.timeline({defaults: {duration: 0.75, ease: 'power1.out'}})
+
+    useEffect(() => {
+        tl.fromTo('#menuModal', {scale: 0}, {scale: 1, ease: "power2.out", duration: 0.5})
+    }, [tl])
     
     return (
         <section 
@@ -18,7 +28,7 @@ export default function MenuModal({onClose, children}: Props) {
             <button className='fixed top-0 right-0 p-8 text-white' onClick={() => onClose()}>
                 <CloseIcon />
             </button>
-            <div className='bg-white w-11/12 md:w-3/5 p-5 rounded-xl md:p-10 max-w-7xl'>
+            <div id='menuModal' className='bg-white w-11/12 md:w-3/5 p-5 rounded-xl md:p-10 max-w-7xl'>
                 {children}
             </div>
         </section>
